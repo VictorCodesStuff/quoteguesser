5d835f74f0f9632c7e8b4aa5a8f9713080e3fe19
VictorCodesStuff <126239044+VictorCodesStuff@users.noreply.github.com>
2026-03-22 15:41:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,295 @@
+const quoteBank = [
+    {
+        speaker: "Romeo",
+        lines: [
+            "It seems she hangs upon the cheek of night As a rich jewel in an Ethiope's ear.",
+            "Arise, fair sun, and kill the envious moon, Who is already sick and pale with grief",
+            "I defy you, stars!",
+            "Why, then, O brawling love, O loving hate",
+            "Love is a smoke made with the fume of sighs",
+            "From Love's weak childish bow she lives uncharmed.",
+            "Not mad, but bound more than a madman is",
+            "O, then, dear saint, let lips do what hands do",
+            "The brightness of her cheek would shame those stars, As daylight doth a lamp.",
+            "What less than doomsday is the prince's doom?",
+            "There is no world without Verona walls, But purgatory, torture, hell itself.",
+            "Every unworthy thing, Live here in heaven and may look on her",
+            "'Tis but the pale reflex of Cynthia's brow;",
+            "I dreamt my lady came and found me dead — Strange dream that gives a dead man leave to think! —",
+            "Famine is in thy cheeks, Need and oppression starveth in thy eyes",
+            "Death, that hath sucked the honey of thy breath, Hath had no power yet upon thy beauty."
+        ]
+    },
+    {
+        speaker: "Juliet",
+        lines: [
+            "Take him and cut him out in little stars.",
+            "My bounty is as boundless as the sea.",
+            "So tedious is this day As is the night before some festival",
+            "This is thy sheath: there rust, and let me die.",
+            "Good pilgrim, you do wrong your hand too much",
+            "Love's heralds should be thoughts, Which ten times faster glide than the sun's beams",
+            "Gallop apace, you fiery-footed steeds, Towards Phoebus' lodging.",
+            "What devil art thou that dost torment me thus?",
+            "Beautiful tyrant, fiend angelical, Dove-feathered raven, wolvish-ravening lamb",
+            "It is not yet near day. It was the nightingale, and not the lark",
+            "Methinks I see thee, now thou art so low, As one dead in the bottom of a tomb.",
+            "Alack, alack, that heaven should practice stratagems Upon so soft a subject as myself!",
+            "Be not so long to speak; I long to die If what thou speak'st speak not of remedy.",
+            "Hide me nightly in a charnel-house, O'ercovered quite with dead men's rattling bones",
+            "Love give me strength, and strength shall help afford.",
+            "What if this mixture do not work at all? Shall I be married then tomorrow morning?",
+            "O churl, drunk all and left no friendly drop To help me after."
+        ]
+    },
+    {
+        speaker: "Tybalt",
+        lines: [
+            "What, art thou drawn among these heartless hinds?",
+            "This, by his voice, should be a Montague.",
+            "Mercutio, thou consortest with Romeo",
+            "Boy, this shall not excuse the injuries. That thou hast done me; therefore turn and draw.",
+            "I will withdraw, but this intrusion shall. Now seeming sweet, convert to bitterest gall.",
+            "Romeo, the love I bear thee can afford. No better term than this,—thou art a villain.",
+            "What, drawn, and talk of peace! I hate the word, As I hate hell, all Montagues, and thee"
+        ]
+    },
+    {
+        speaker: "Benvolio",
+        lines: [
+            "What sadness lengthens Romeo's hours?",
+            "Alas that Love, so gentle in his view, Should be so tyrannous and rough in proof!",
+            "I'll pay that doctrine, or else die in debt",
+            "One desperate grief cures with another's languish",
+            "There lies the man, slain by young Romeo, That slew thy kinsman, brave Mercutio.",
+            "Part, fools, put up your swords; you know not what you do.",
+            "In the instant came The fiery Tybalt, with his sword prepared",
+            "A right fair mark, fair coz, is soonest hit.",
+            "Tut, man, one fire burns out another's burning; One pain is lessened by another's anguish"
+        ]
+    },
+    {
+        speaker: "Paris",
+        lines: [
+            "But now, my lord, what say you to my suit?",
+            "My lord, I would that Thursday were tomorrow.",
+            "Venus smiles not in a house of tears.",
+            "These times of woe afford no time to woo.",
+            "Thy face is mine, and thou hast slandered it.",
+            "Till then, adieu, and keep this holy kiss.",
+            "Have I thought long to see this morning's face, And doth it give me such a sight as this?",
+            "O love! O life! Not life, but love in death!",
+            "Sweet flower, with flowers thy bridal bed I strew, — O woe, thy canopy is dust and stones!  —",
+            "Stop thy unhallowed toil, vile Montague! Can vengeance be pursued further than death?"
+        ]
+    },
+        {
+        speaker: "Lord Capulet",
+        lines: [
+            "My child is yet a stranger in the world;",
+            "The earth hath swallowed all my hopes but she",
+            "Hang thee, young baggage, disobedient wretch!",
+            "I promise you, but for your company, I would have been abed an hour ago.",
+            "I think she will be ruled In all respects by me.",
+            "A Thursday, tell her, She shall be married to this noble earl.",
+            "How, will she none? Doth she not give us thanks?Is she not proud?",
+            "Graze where you will, you shall not house with me.",
+            "I'll not to bed tonight, let me alone; I'll play the housewife for this once.",
+            "Death lies on her like an untimely frost Upon the sweetest flower of all the field."
+        ]
+    },
+    {
+        speaker: "Lady Capulet",
+        lines: [
+            "I have remembered me, thou's hear our counsel.",
+            "Read o'er the volume of young Paris' face",
+            "Tonight she is mewed up to her heaviness.",
+            "Prince, as thou art true, For blood of ours shed blood of Montague.",
+            "Some twenty of them fought in this black strife, And all those twenty could but kill one life.",
+            "The County Paris, at Saint Peter's Church, Shall happily make thee there a joyful bride.",
+            "Ay, you have been a mouse-hunt in your time; But I will watch you from such watching now.",
+            "Alack the day, she's dead, she's dead, she's dead!",
+            "O me, this sight of death is as a bell That warns my old age to a sepulchre."
+        ]
+    },
+    {
+        speaker: "Nurse",
+        lines: [
+            "Nay, he's a flower; in faith, a very flower",
+            "His name is Romeo, and a Montague, The only son of your great enemy.",
+            "I pray you, sir, what saucy merchant was this, that was so full of his ropery?",
+            "Now, afore God, I am so vexed, that every part about me quivers.",
+            "Fie, how my bones ache! What a jaunce have I!",
+            "I'll warrant him as gentle as a lamb.",
+            "Ah, weraday, he's dead, he's dead, he's dead!",
+            "O courteous Tybalt, honest gentleman, That ever I should live to see thee dead!",
+            "There's no trust, No faith, no honesty in men",
+            "Stand up, stand up, stand an you be a man. For Juliet's sake, for her sake, rise and stand.",
+            "An eagle, madam, Hath not so green, so quick, so fair an eye As Paris hath.",
+            "Why, lamb! Why, lady. Fie, you slug-a-bed!"
+        ]
+    },
+    {
+        speaker: "Friar Laurence",
+        lines: [
+            "O deadly sin! O rude unthankfulness!",
+            "The earth that's nature's mother is her tomb; What is her burying grave, that is her womb.",
+            "Lo, here upon thy cheek the stain doth sit Of an old tear that is not washed off yet.",
+            "Wisely and slow; they stumble that run fast.",
+            "And turned that black word 'death' to 'banishment.' This is dear mercy and thou seest it not.",
+            "Thou fond mad man, hear me a little speak.",
+            "Thy tears are womanish; thy wild acts denote The unreasonable fury of a beast.",
+            "Thy noble shape is but a form of wax, Digressing from the valor of a man",
+            "Take thou this vial, being then in bed, And this distilling liquor drink thou off.",
+            "Heaven and yourself Had part in this fair maid; now heaven hath all, And all the better is it for the maid.",
+            "Poor living corpse, closed in a dead man's tomb!"
+        ]
+    },
+    {
+        speaker: "Mercutio",
+        lines: [
+            "Borrow Cupid's wings, And soar with them above a common bound.",
+            "If love be rough with you, be rough with love;",
+            "We waste our lights in vain, like lamps by day.",
+            "O, then I see Queen Mab hath been with you;",
+            "The ape is dead, and I must conjure him.",
+            "If love be blind, love cannot hit the mark.",
+            "Any man that can write may answer a letter.",
+            "O, he is the courageous captain of compliments.",
+            "A plague o' both your houses! I am sped"
+        ]
+    },
+        {
+        speaker: "fake",
+        lines: [
+            "Something is rotten in the state of Denmark.",
+            "Doubt that the sun doth move; Doubt truth to be a liar; But never doubt I love.",
+            "God hath given you one face, and you make yourself another.",
+            "All the world's a stage, and all the men and women merely players.",
+            "Is this a dagger which I see before me, the handle toward my hand?",
+            "Love looks not with the eyes, but with the mind; and therefore is winged Cupid painted blind.",
+            "Thou art as useful as a lantern in broad daylight.",
+            "Love is a tempest, and I am but a soggy loaf within it.",
+            "My sorrow is deep, yet deeper still is my desire for a nap.",
+            "O traitor, thou hast devoured my final pancake.",
+            "Thy love is as a gentle zephyr's breath, which hath my hat o'erthrown.",
+            "My heart is full of sorrow, and my stomach empty."
+        ]
+    }
+];
+
+let currentCharacter;
+let currentQuoteIndex;
+let attempts;
+let usedQuoteIndexes = [];
+let timeElapsed = 0;
+let timerInterval;
+let roundActive = true;
+
+function pickRandomCharacter() {
+    const index = Math.floor(Math.random() * quoteBank.length);
+    return quoteBank[index];
+}
+
+function startRound() {
+    clearInterval(timerInterval);
+    timeElapsed = 0;
+    document.getElementById("timer").textContent = "Time: " + timeElapsed;
+
+    timerInterval = setInterval(() => {
+        timeElapsed++;
+        document.getElementById("timer").textContent = "Time: " + timeElapsed;
+    }, 1000);
+
+    currentCharacter = pickRandomCharacter();
+    currentQuoteIndex = Math.floor(Math.random() * currentCharacter.lines.length);
+    usedQuoteIndexes = [currentQuoteIndex];
+    attempts = 0;
+    const cards = document.querySelectorAll(".card");
+    cards.forEach(card => {
+        card.classList.remove("disabled")
+    });
+    document.getElementById("quote1").textContent = "(Quote hidden)";
+    document.getElementById("quote2").textContent = "(Quote hidden)";
+    document.getElementById("quote3").textContent = "(Quote hidden)";
+    document.getElementById("quote1").className = "quote hidden-text";
+    document.getElementById("quote2").className = "quote hidden-text";
+    document.getElementById("quote3").className = "quote hidden-text";
+
+    revealQuote(document.getElementById("quote1"), currentCharacter.lines[currentQuoteIndex])
+}
+
+function handleGuess(card) {
+    const guess = card.dataset.name;
+    if (guess === currentCharacter.speaker) {
+        clearInterval(timerInterval);
+        showResult("Correct! It was " + currentCharacter.speaker);
+    } else {
+        card.classList.add("disabled");
+        attempts++;
+
+        if (attempts === 1) {
+            const newIndex = getNewQuoteIndex();
+            const q2 = document.getElementById("quote2");
+            revealQuote(q2, currentCharacter.lines[newIndex]);
+        }
+        else if (attempts === 2) {
+            const newIndex = getNewQuoteIndex();
+            const q3 = document.getElementById("quote3");
+            revealQuote(q3, currentCharacter.lines[newIndex]);
+        }
+        else {
+            clearInterval(timerInterval);
+            showResult("Wrong! It was " + currentCharacter.speaker);        
+        }
+    }
+}
+
+function getNewQuoteIndex() {
+    let newIndex = Math.floor(Math.random()*currentCharacter.lines.length);
+
+    while (usedQuoteIndexes.includes(newIndex)) {
+        newIndex = Math.floor(Math.random() * currentCharacter.lines.length);
+    }
+
+    usedQuoteIndexes.push(newIndex);
+    return newIndex;
+}
+
+function revealQuote(element, text){
+    element.style.opacity = 0;
+    setTimeout(() => {
+        element.textContent = text;
+        element.className = "quote revealed";
+        element.style.opacity = 1;
+
+    }, 400);
+}
+
+startRound();
+
+const cards = document.querySelectorAll(".card");
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        if (!roundActive) return;
+        handleGuess(card);
+    });
+});
+
+function showResult(message) {
+    roundActive = false;
+    document.getElementById("quote1").textContent = "";
+    document.getElementById("quote2").textContent = "";
+    document.getElementById("quote3").textContent = "";
+
+    document.getElementById("quote1").textContent = message;
+    document.getElementById("quote1").className = "quote revealed";
+    document.getElementById("next-round-btn").classList.remove("hidden");
+}
+
+document.getElementById("next-round-btn").addEventListener("click", () => {
+    document.getElementById("next-round-btn").classList.add("hidden");
+    roundActive = true;
+    startRound();
+});
+
